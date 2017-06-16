f690013906b6ebc3aff9ca73baf434285c7df0f0
Murphy <Ryan.P.Murphy@emc.com>
2017-06-16 10:06:39 -0400

    Initial commit v1.0

---
+++ GenerateMetaDestroyScript/Program.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Xml;
+
+namespace GenerateMetaDestroyScript
+{
+    class Program
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            #region Variables
+
+            var vmaxSerialNumbers = new List<string>();
+            string userSerialNumber = null;
+            string userMetaVolumes = null;
+            List<string> devicesToScript;
+            SymCliHelper helper = new SymCliHelper();
+            string desktopPath;
+            string fullPath;
+
+     
+            XmlDocument doc;
+            XmlNodeList nodeList;
+            XmlNode node;
+
+
+            #endregion
+
+            //Print the porgram header
+            Console.WriteLine("VMAX Meta Device Cleanup Script Creator - v1.0");
+            Console.WriteLine();
+
+            Console.WriteLine("[1] - Checking what VMAX's are in the symapi database.");
+            Console.WriteLine();
+            System.Threading.Thread.Sleep(500);
+
+            if (args.Length > 1)
+            {
+                userSerialNumber = args[0];
+                userMetaVolumes = args[1];
+            }
+            else
+            {
+                // Determine what VMAX arrays symcli see's by generate a node list of all symmetrix entries from the symcli output
+                doc = helper.runSymCliCommand("symcfg.exe", "list -output xml_e");
+                nodeList = doc.SelectNodes("/SymCLI_ML/Symmetrix");
+                Console.WriteLine("There are: " + nodeList.Count + " VMAX arrays visible: ");
+
+                //Sort through the symmetrix entries and extract each serial number
+                int counter = 1;
+                foreach (XmlNode xn in nodeList)
+                {
+                    XmlNode symmNode = xn.SelectSingleNode("Symm_Info/symid");
+                    Console.WriteLine("(" + counter + ") - " + symmNode.InnerText);
+                    counter++;
+                }
+                
+                //Prompt the user for what serial number and metadevice they would like to create a script for
+                Console.WriteLine();
+                Console.Write("Enter the last four digits of the VMAX serial number: ");
+                userSerialNumber = Console.ReadLine();
+                Console.Write("Enter the meta volume(s) you wish to destroy: ");
+                userMetaVolumes = Console.ReadLine();
+                Console.WriteLine();
+            }
+
+            //split out devices into a list of devices to be checked and scripted
+            devicesToScript = new List<string>(userMetaVolumes.Split(','));
+
+            //Set the desktop path
+            desktopPath = helper.setDesktopPath();
+
+            // Write the commands to a file.
+            Console.WriteLine("[2] - Creating commands script file at: " + desktopPath);
+            fullPath = helper.createCommandScript(desktopPath, userSerialNumber);
+            Console.WriteLine();
+
+            //for each volume passed to the cli
+            int devCounter = 1;
+            foreach (string userMetaVolume in devicesToScript)
+            {
+                Console.WriteLine("[Device " + devCounter + "] - Creating commands for " + userMetaVolume + ".");
+                Dictionary<string, string> volumeAttributes;
+                List<string> metaMembers;
+                List<string> previewCommands;
+                List<string> commitCommands;
+                Boolean isMeta = false;
+                Boolean hasRDF = false;
+                Boolean hasClone = false;
+                Boolean isMapped = false;
+
+                //Load the symcli output into a XML document so that we can parse through to find the nodes we need
+                doc = helper.runSymCliCommand("symdev.exe", "-sid " + userSerialNumber + " show " + userMetaVolume + " -output xml_e -offline");
+
+                //Begin device checks
+                node = doc.SelectSingleNode("SymCLI_ML/Symmetrix/Device");
+
+                //Check if the device is a meta
+                Console.WriteLine("[Device " + devCounter + "] - Checking if " + userMetaVolume + " is a meta.");
+                isMeta = helper.checkDeviceConfig(node, "Meta");
+                System.Threading.Thread.Sleep(500);
+
+                //Check if the device is mapped down any FA's
+                Console.WriteLine("[Device " + devCounter + "] - Checking if " + userMetaVolume + " is masked/mapped.");
+                isMapped = helper.checkDeviceConfig(node, "Front_End");
+                System.Threading.Thread.Sleep(500);
+
+                //Check if the device has a BCV
+                Console.WriteLine("[Device " + devCounter + "] - Checking if " + userMetaVolume + " is part of a TimeFinder session.");
+                hasClone = helper.checkDeviceConfig(node, "CLONE_Device");
+                System.Threading.Thread.Sleep(500);
+
+                //Check if the device has RDF
+                Console.WriteLine("[Device " + devCounter + "] - Checking if " + userMetaVolume + " is part of an RDF session.");
+                Console.WriteLine();
+                hasRDF = helper.checkDeviceConfig(node, "RDF");
+                System.Threading.Thread.Sleep(500);
+
+                //Perform check on LUN, do not continue if it is not a meta, else continute with warnings
+                if (isMeta == false)
+                {
+                    Console.WriteLine("[Device " + devCounter + "][Error] - " + userMetaVolume + " is not a meta device. The program will now terminate.");
+                    Console.WriteLine();
+                }
+                else
+                {
+                    //find meta members under meta informaiton
+                    node = doc.SelectSingleNode("SymCLI_ML/Symmetrix/Device/Meta");
+                    metaMembers = helper.getMetaMembers(node);
+
+                    //Populate the attributes directory with information on the meta device
+                    volumeAttributes = helper.getDeviceAttributes(doc);
+
+                    //Console.WriteLine("[5] - Generating script to destory the meta volume " + userMetaVolume + " from VMAX " + userSerialNumber + ".");
+                    //System.Threading.Thread.Sleep(500);
+
+                    //Generate Commands
+                    previewCommands = helper.generateCleanupCommands("preview", userSerialNumber, userMetaVolume, volumeAttributes, metaMembers);
+                    commitCommands = helper.generateCleanupCommands("commit", userSerialNumber, userMetaVolume, volumeAttributes, metaMembers);
+
+
+                    //Write commands to file
+                    helper.writeCommandScript(fullPath, userSerialNumber, previewCommands, commitCommands);
+
+                    //Print warnings if the device is mapped, part of a BCV session, or RDF session
+                    if (isMapped == true)
+                        Console.WriteLine("[Device " + devCounter + "][Warning] - " + userMetaVolume + " is mapped down FA ports. You will need to unmask/umap it before running the cleanup commands.");
+                        System.Threading.Thread.Sleep(500);
+                    if (hasClone == true)
+                        Console.WriteLine("[Device " + devCounter + "][Warning] - " + userMetaVolume + "  is part of a TimeFinder session. You will need remove the TimeFinder relationship before running the cleanup commands.");
+                        System.Threading.Thread.Sleep(500);
+                    if (hasRDF == true)
+                        Console.WriteLine("[Device " + devCounter + "][Warning] - " + userMetaVolume + "  is part of a RDF session. You will need remove the SRDF relationship before running the cleanup commands.");
+                        System.Threading.Thread.Sleep(500);
+
+                }//end if check for meta or not
+                devCounter++;
+            }
+            Console.WriteLine("[3] - Script generation complete, you can now close the command line window.");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ GenerateMetaDestroyScript/SymCliHelper.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace GenerateMetaDestroyScript
+{
+    class SymCliHelper
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="command"></param>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        public XmlDocument runSymCliCommand(string command, string args)
+        {
+            Process cliProcess = new Process();
+            cliProcess.StartInfo.UseShellExecute = false;
+            cliProcess.StartInfo.RedirectStandardOutput = true;
+            cliProcess.StartInfo.FileName = command;
+            cliProcess.StartInfo.Arguments = args;
+            cliProcess.Start();
+            string output = cliProcess.StandardOutput.ReadToEnd();
+            cliProcess.WaitForExit();
+
+            //Debug - Write command output to console
+            //Console.WriteLine(output);
+
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(output);
+            return doc;
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="node"></param>
+        /// <returns></returns>
+        public Boolean checkDeviceConfig(XmlNode node, string nodeType)
+        {
+            Boolean result = false;
+            XmlNodeList nodeList = node.SelectNodes(nodeType);
+
+            if (nodeList.Count > 0)
+            {
+                if (nodeList[0].ChildNodes.Count > 1)
+                {
+                    // Console.WriteLine("Meta list: " + nodeList[0].ChildNodes.Count);
+                    result = true;
+                }
+            }
+            return result;    
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="doc"></param>
+        /// <returns></returns>
+        public Dictionary<string, string> getDeviceAttributes(XmlDocument doc)
+        {
+            Dictionary<string, string> volumeAttributes = new Dictionary<string, string>();
+            volumeAttributes.Add("devMetaID", doc.SelectSingleNode("SymCLI_ML/Symmetrix/Device/Dev_Info/dev_name").InnerXml);
+            volumeAttributes.Add("devType", doc.SelectSingleNode("SymCLI_ML/Symmetrix/Device/Dev_Info/configuration").InnerXml);
+            volumeAttributes.Add("devBoundPool", doc.SelectSingleNode("SymCLI_ML/Symmetrix/Device/Dev_Info/thin_pool_name").InnerXml);
+            volumeAttributes.Add("devMetaLocation", doc.SelectSingleNode("SymCLI_ML/Symmetrix/Device/Flags/meta").InnerXml);
+            volumeAttributes.Add("metaType", doc.SelectSingleNode("SymCLI_ML/Symmetrix/Device/Meta/configuration").InnerXml);
+            volumeAttributes.Add("metaMemberCount", doc.SelectSingleNode("SymCLI_ML/Symmetrix/Device/Meta/members").InnerXml);
+
+            return volumeAttributes;
+
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="node"></param>
+        /// <returns></returns>
+        public List<string> getMetaMembers(XmlNode node)
+        {
+            List<string> metaMembers = new List<string>();
+
+            foreach (XmlNode xmlNode in node)
+            {
+                if (xmlNode.FirstChild.LocalName == "dev_name")
+                {
+                    metaMembers.Add(xmlNode.FirstChild.InnerXml);
+                }
+            }
+            return metaMembers;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="userSN"></param>
+        /// <param name="userDev"></param>
+        /// <param name="volumeAttributes"></param>
+        /// <param name="metaMembers"></param>
+        /// <returns></returns>
+        public List<string> generateCleanupCommands(string type, string userSN, string userDev, Dictionary<string, string> volumeAttributes, List<string> metaMembers)
+        {
+            List<string> commands = new List<string>();
+            commands.Add("symconfigure -sid " + userSN + " -cmd \"unbind tdev " + userDev + " from pool " + volumeAttributes["devBoundPool"] + ";\" " + type);
+            commands.Add("symconfigure -sid " + userSN + " -cmd \"dissolve meta dev " + userDev + ";\" " + type);
+            foreach (string tdev in metaMembers)
+            {
+                commands.Add("symconfigure -sid " + userSN + " -cmd \"delete dev " + tdev + ";\" " + type);
+            }
+            return commands;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="commands"></param>
+        public void printCleanupCommands(List<string> commands)
+        {
+           Console.WriteLine();
+           foreach (string command in commands)
+            {
+                Console.WriteLine(command);
+            }
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public string setDesktopPath()
+        {
+            string desktopPath;
+            if (Environment.OSVersion.Version.Equals("6.2.9200.0"))
+            {
+                desktopPath = @"C:\Users\" + Environment.UserName + @"\Desktop\";
+            }
+            else
+            {
+                desktopPath = @"C:\Documents and Settings\" + Environment.UserName + @"\Desktop\";
+            }
+            return desktopPath;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="desktopPath"></param>
+        /// <param name="userSerialNumber"></param>
+        public string createCommandScript(string desktopPath,string userSerialNumber)
+        {
+            string fullpath = desktopPath + DateTime.Now.DayOfYear + DateTime.Now.Hour + DateTime.Now.Minute + DateTime.Now.Second + DateTime.Now.Millisecond + "_VMAX" + userSerialNumber;
+            System.IO.StreamWriter file = new System.IO.StreamWriter(fullpath + "_preview_commands.txt");
+            file.WriteLine("# Preview Commands");
+            file.Close();
+
+            System.IO.StreamWriter file2 = new System.IO.StreamWriter(fullpath + "_commit_commands.txt");
+            file2.WriteLine("# Commit Commands");
+            file2.Close();
+            return fullpath;
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="desktopPath"></param>
+        /// <param name="userSerialNumber"></param>
+        /// <param name="previewCommands"></param>
+        /// <param name="commitCommands"></param>
+        public void writeCommandScript(string fullPath, string userSerialNumber, List<string> previewCommands, List<string> commitCommands)
+        {
+            System.IO.StreamWriter file = new System.IO.StreamWriter(fullPath + "_preview_commands.txt", append: true);
+            foreach (string command in previewCommands)
+            {
+                file.WriteLine(command);
+            }
+            file.Close();
+
+            System.IO.StreamWriter file2 = new System.IO.StreamWriter(fullPath + "_commit_commands.txt", append: true);
+            foreach (string command in commitCommands)
+            {
+                file2.WriteLine(command);
+            }
+            file2.Close();
+        }
+    }
+}
